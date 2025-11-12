3dea07a71525cf27b0ef770d503518af8b52f830
Giancarlo <gbuchelli98@gmail.com>
2025-11-12 16:29:53 -0500

    modificado para eliminar resenas de un juego cuando se elimina un juego

--- src/models/game.js
+++ src/models/game.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import Review from "./review.js"; // importa el modelo de Review
 
 const gameSchema = new mongoose.Schema(
     {
@@ -14,5 +15,15 @@ const gameSchema = new mongoose.Schema(
     },
     { versionKey: false }
 );
+
+// Middleware: borrar reseñas asociadas al eliminar un juego
+gameSchema.pre("findOneAndDelete", async function (next) {
+  const game = await this.model.findOne(this.getFilter());
+  if (game) {
+    await Review.deleteMany({ juegoId: game._id });
+    console.log(`🧹 Reseñas del juego "${game.titulo}" eliminadas.`);
+  }
+  next();
+});
 
 export default mongoose.model("Game", gameSchema);
